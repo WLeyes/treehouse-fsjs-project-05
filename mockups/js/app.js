ba47632831be3a3e600c6c576fcf8701475ca31b
Warren Leyes <warren.leyes@gmail.com>
2018-09-16 10:24:17 -0700

    attached js to html, setup basic module pattern

---
+++ mockups/js/app.js
@@ -0,0 +1,18 @@
+// UI Controller
+const UICtrl = (() => {
+  
+})();
+
+
+// Data Controller
+const DataCtrl = (() => {
+
+})();
+
+// App Controller
+const App = ((UICtrl, DataCtrl) => {
+  
+})(UICtrl, DataCtrl);
+
+// Initialize App
+App.init()
